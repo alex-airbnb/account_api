afe9f5c4d6b7e8e5fb79def2494dfb058ae4941d
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-02-17 19:50:50 -0600

    feat: Add Account and CreateAccount to the package entity.

---
+++ entity/account_test.go
@@ -0,0 +1,47 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/franela/goblin"
+)
+
+func TestAccount(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	g.Describe("Account", func() {
+		g.Describe("CreateAccount", func() {
+			g.Describe("when the email, lastName, and name are valid", func() {
+				g.It("it should create a account", func() {
+					expectedAccount := Account{
+						Email:    "account@mail.com",
+						LastName: "Account Last Name",
+						Name:     "Account Name",
+					}
+
+					currentAccount, err := CreateAccount(
+						expectedAccount.Email,
+						expectedAccount.LastName,
+						expectedAccount.Name,
+					)
+
+					g.Assert(currentAccount).Equal(expectedAccount)
+					g.Assert(err).Equal(nil)
+				})
+			})
+
+			g.Describe("when the email is invalid", func() {
+				g.It("it should return InvalidEmail error", func() {
+					currentAccount, err := CreateAccount(
+						"Email",
+						"Last Name",
+						"Name",
+					)
+
+					g.Assert(currentAccount).Equal(Account{})
+					g.Assert(err).Equal(ErrInvalidEmail)
+				})
+			})
+		})
+	})
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/alex-airbnb/account_api
 
 go 1.15
 
-require github.com/franela/goblin v0.0.0-20210113153425-413781f5e6c8
+require (
+	github.com/franela/goblin v0.0.0-20210113153425-413781f5e6c8
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/sys v0.0.0-20210217105451-b926d437f341 // indirect
+)

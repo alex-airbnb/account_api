46505cfdd84e4a36f35feffb92745061a03a1312
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-02-21 14:35:50 -0600

    feat: Add RepositoryPort to the adapter package.

--- usecase/account_rest_test.go
+++ usecase/account_rest_test.go
@@ -1,33 +1,71 @@
 package usecase
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/alex-airbnb/account_api/entity"
 	"github.com/franela/goblin"
 )
 
+var createMockFunction func(model interface{}) error
+
+type repositoryMock struct{}
+
+func (r *repositoryMock) Create(model interface{}) error {
+	return createMockFunction(model)
+}
+
 func TestAccount(t *testing.T) {
 	g := goblin.Goblin(t)
 
 	g.Describe("CreateAccountREST Use Case", func() {
 		g.Describe("when the request is valid", func() {
-			g.It("it should create an account and return a result", func() {
-				a := &AccountREST{}
-				expectedResult := CreateAccountResponse{
-					Email: "account@mail.com",
-					ID:    0,
-				}
-				req := []byte(`{
-					"email": "account@mail.com",
-					"lastName": "lastName",
-					"name": "name"
-				}`)
+			g.Describe("when the Repository can save the account", func() {
+				g.It("it should create an account and return a result", func() {
+					a := &AccountREST{
+						Repository: &repositoryMock{},
+					}
+					createMockFunction = func(m interface{}) error {
+						return nil
+					}
+					expectedResult := CreateAccountResponse{
+						Email: "account@mail.com",
+						ID:    0,
+					}
+					req := []byte(`{
+						"email": "account@mail.com",
+						"lastName": "lastName",
+						"name": "name"
+					}`)
 
-				currentResult, err := a.CreateAccount(req)
+					currentResult, err := a.CreateAccount(req)
 
-				g.Assert(currentResult).Equal(expectedResult)
-				g.Assert(err).Equal(nil)
+					g.Assert(currentResult).Equal(expectedResult)
+					g.Assert(err).Equal(nil)
+				})
+			})
+
+			g.Describe("when the Repository can't save the account", func() {
+				g.It("it should return RepositoryCreate error", func() {
+					a := &AccountREST{
+						Repository: &repositoryMock{},
+					}
+					errCreate := errors.New("Repository Create Error")
+					createMockFunction = func(m interface{}) error {
+						return errCreate
+					}
+					req := []byte(`{
+						"email": "account@mail.com",
+						"lastName": "lastName",
+						"name": "name"
+					}`)
+
+					currentResult, err := a.CreateAccount(req)
+
+					g.Assert(currentResult).Equal(CreateAccountResponse{})
+					g.Assert(err).Equal(errCreate)
+				})
 			})
 		})
 

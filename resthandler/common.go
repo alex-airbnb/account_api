a423a0efd96783dfdfc5696864d2cbc979815521
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-03-02 18:55:33 -0600

    perf: Replace Setup by NewRESTHandler function.

--- resthandler/common.go
+++ resthandler/common.go
@@ -5,14 +5,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-var createMockFunction func(model interface{}) error
-
-type repositoryMock struct{}
-
-func (r *repositoryMock) Create(model interface{}) error {
-	return createMockFunction(model)
-}
-
 type response struct {
 	Data       int    `json:"data"`
 	Error      bool   `json:"error"`
@@ -21,10 +13,9 @@ type response struct {
 	StatusCode int    `json:"statusCode"`
 }
 
-// Setup Set up a new Fiber App.
-func Setup() *fiber.App {
+// NewRESTHandler Set up a new Fiber App.
+func NewRESTHandler(accountUseCase usecase.UseCase) *fiber.App {
 	app := fiber.New()
-	accountUseCase := usecase.NewAccountREST(&repositoryMock{})
 	accountGroup := app.Group("/account")
 
 	AccountRouter(accountGroup, accountUseCase)

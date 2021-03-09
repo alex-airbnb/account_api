bffcdafffb9d49b94deef06f2203d080907e2a20
AlexisNava <brian.alexis.nava.arreola@gmail.com>
2021-03-08 19:46:03 -0600

    feat: Add and run the REST Handler.

--- main.go
+++ main.go
@@ -1,6 +1,48 @@
 package main
 
-// Add Add two numbers
-func Add(number1, number2 int) int {
-	return number1 + number2
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/alex-airbnb/account_api/adapter"
+	"github.com/alex-airbnb/account_api/client"
+	"github.com/alex-airbnb/account_api/resthandler"
+	"github.com/alex-airbnb/account_api/usecase"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Panic(err)
+	}
+
+	go func() {
+		if err := client.SetUpPostgres(); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	app := resthandler.NewRESTHandler(
+		usecase.NewAccountREST(
+			adapter.NewPostgres(client.Postgres),
+		),
+	)
+
+	go func() {
+		if err := app.Listen(":3000"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	shutdownChannnel := make(chan os.Signal)
+
+	signal.Notify(shutdownChannnel, os.Interrupt)
+	signal.Notify(shutdownChannnel, os.Kill)
+
+	shutdownNotification := <-shutdownChannnel
+
+	log.Println("Received Shutdown Notification", shutdownNotification)
+
+	app.Shutdown()
 }
